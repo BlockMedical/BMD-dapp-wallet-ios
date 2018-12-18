8aab265343a26a3b09b984b6e1d7d1be81221f2d
Henry Fan <abcdz330@gmail.com>
2018-12-19 07:25:20 +0800

    Set blockMed default gas as 6 gwei

--- Trust/Transfer/Types/GasPriceConfiguration.swift
+++ Trust/Transfer/Types/GasPriceConfiguration.swift
@@ -7,4 +7,5 @@ public struct GasPriceConfiguration {
     static let `default`: BigInt = EtherNumberFormatter.full.number(from: "24", units: UnitConfiguration.gasPriceUnit)!
     static let min: BigInt = EtherNumberFormatter.full.number(from: "1", units: UnitConfiguration.gasPriceUnit)!
     static let max: BigInt = EtherNumberFormatter.full.number(from: "100", units: UnitConfiguration.gasPriceUnit)!
+    static let blockMedDefault: BigInt = EtherNumberFormatter.full.number(from: "6", units: UnitConfiguration.gasPriceUnit)!
 }
