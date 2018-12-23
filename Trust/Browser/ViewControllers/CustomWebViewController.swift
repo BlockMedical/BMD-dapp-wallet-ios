6f65c3969ab24991da40e0e83d087bc304040148
Henry Fan <abcdz330@gmail.com>
2018-12-23 14:50:08 +0800

    Add webView layout in CustomWeb

--- Trust/Browser/ViewControllers/CustomWebViewController.swift
+++ Trust/Browser/ViewControllers/CustomWebViewController.swift
@@ -1,8 +1,15 @@
 // Copyright DApps Platform Inc. All rights reserved.
 
 import UIKit
+import WebKit
 
 class CustomWebViewController: UIViewController {
+
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
 
     // MARK: - Lifecycle
 
@@ -11,6 +18,17 @@ class CustomWebViewController: UIViewController {
 
     init(url: URL) {
         super.init(nibName: nil, bundle: nil)
+
+        view.backgroundColor = .white
+        view.addSubview(webView)
+
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor),
+        ])
+        webView.load(URLRequest(url: url))
     }
 
     required init?(coder aDecoder: NSCoder) {
