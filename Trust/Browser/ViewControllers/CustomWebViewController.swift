a0a41cdc0fc7a012e37d5c2d9c53db7b981fbaff
Henry Fan <abcdz330@gmail.com>
2018-12-25 07:48:32 +0800

    Add configuration and indicator layout on CustomWeb

--- Trust/Browser/ViewControllers/CustomWebViewController.swift
+++ Trust/Browser/ViewControllers/CustomWebViewController.swift
@@ -5,10 +5,25 @@ import WebKit
 
 class CustomWebViewController: UIViewController {
 
+    private lazy var webViewConfiguration: WKWebViewConfiguration = {
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+        return configuration
+    }()
+
     private lazy var webView: WKWebView = {
-        let webView = WKWebView(frame: .zero)
+        let webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
+        webView.navigationDelegate = self
+        webView.isUserInteractionEnabled = true
         webView.translatesAutoresizingMaskIntoConstraints = false
         return webView
+    }()
+
+    private lazy var indicatorView: UIActivityIndicatorView = {
+        let indicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        indicatorView.hidesWhenStopped = true
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        return indicatorView
     }()
 
     // MARK: - Lifecycle
@@ -21,12 +36,15 @@ class CustomWebViewController: UIViewController {
 
         view.backgroundColor = .white
         view.addSubview(webView)
+        view.addSubview(indicatorView)
 
         NSLayoutConstraint.activate([
             webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor),
+            indicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            indicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
         webView.load(URLRequest(url: url))
     }
@@ -37,6 +55,23 @@ class CustomWebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+}
+
+extension CustomWebViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicatorView.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        indicatorView.startAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("webView didFail : \(error)")
+        indicatorView.stopAnimating()
     }
 
 }
